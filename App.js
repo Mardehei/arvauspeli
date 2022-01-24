06c643cd8a98dc9bcad10d304aab418603483b51
Mardehei <82866317+Mardehei@users.noreply.github.com>
2022-01-24 21:03:19 +0200

    tehtävä toimii

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
+import React from "react";
 
 export default function App() {
+
+  const [number, setNumber] = React.useState('');
+  const [result, setResult] = React.useState('');
+  const [random, setRandom] = React.useState(Math.floor(Math.random() * 100) + 1);
+  const [guesses, setGuesses] = React.useState(0);
+
+  const checkGuess = () => {
+
+    const totalGuesses = guesses + 1;
+    setGuesses(totalGuesses);
+
+    if (number == random) {
+      Alert.alert("You guessed the correct number! It took " + totalGuesses + " guesses")
+    } else if (number > random) {
+      setResult('Your guess ' + number + ' is too high');
+    } else {
+      setResult('Your guess ' + number + ' is too low');
+    } 
+  }
+
   return (
     <View style={styles.container}>
-      <Text>meneekö githubiin</Text>
-      <Text>Githubiin</Text>
-      <Text>Lisää testiä</Text>
-      <StatusBar style="auto" />
+      <Text>Guess a number between 1-100</Text>
+      <Text>{result}</Text>
+      <TextInput
+          style={styles.input}
+          onChangeText={number => setNumber(number)}
+          value={number}
+          placeholder="Type your guess here"
+          keyboardType="numeric"
+        />
+      <View style={{ flexDirection: "row" }}>
+      <Button
+        title="Make a guess"
+        color="#2196F3"
+        onPress={checkGuess}
+      />
+      </View>
     </View>
   );
 }
